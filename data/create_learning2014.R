6d25a9aca3841c6dd08be02f0cf4f1e2044bd55f
guinv8 <guilherme.varro@helsinki.fi>
2017-01-30 23:42:38 +0200

    Data Wrangling Exercise [Exercise 2]

---
+++ data/create_learning2014.R
@@ -0,0 +1,18 @@
+"Guilherme Varro, Jan 29th 2017. Data Wrangling Exercise - Linear Regression 2"
+learning2014 <- read.table("http://www.helsinki.fi/~kvehkala/JYTmooc/JYTOPKYS3-data.txt", sep="\t", header=TRUE)
+#The data learning 2014 dimension is of 183x60#
+str(learning2014)
+deep_questions <- c("D03", "D11", "D19", "D27", "D07", "D14", "D22", "D30","D07","D14","D22","D30")
+surface_questions <- c("SU02","SU10","SU18","SU26", "SU05","SU13","SU21","SU29","SU08","SU16","SU24","SU32")
+strategic_questions <- c("ST01","ST09","ST17","ST25","ST04","ST12","ST20","ST28")
+library(dplyr)
+deep_columns <- select(learning2014, one_of(deep_questions))
+learning2014$deep <- rowMeans(deep_columns)
+surface_columns <- select(learning2014, one_of(surface_questions))
+learning2014$surf <- rowMeans(surface_columns)
+strategic_columns <- select(learning2014, one_of(strategic_questions))
+learning2014$stra <- rowMeans(strategic_columns)
+keep_columns <- c("gender","Age","Attitude", "deep", "stra", "surf", "Points")
+learning2014 <- select(learning2014, one_of(keep_columns))
+learning2014 <- filter(learning2014, Points>0)
+write.table(learning2014,file="learning2014.txt") 
